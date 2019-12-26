602941649eaae6c459bb5700a6eb514914eb1153
alvian <alvian.ardhiansyah@gmail.com>
2019-12-26 23:00:05 +0700

    revisi

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -24,15 +24,8 @@
     Argon Dashboard - Free Dashboard for Bootstrap 4 by Creative Tim
   </title>
   <!-- Favicon -->
-  <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-  <link href="{{ asset('assets/img/brand/favicon.png') }}" rel="icon" type="image/png">
-  <!-- Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
-  <!-- Icons -->
-  <link href="{{ asset('assets/js/plugins/nucleo/css/nucleo.css') }}" rel="stylesheet" />
-  <link href="{{ asset('assets/js/plugins/@fortawesome/fontawesome-free/css/all.min.css') }}" rel="stylesheet" />
-  <!-- CSS Files -->
-  <link href="{{ asset('assets/css/argon-dashboard.css?v=1.1.0') }}" rel="stylesheet" />
+  {{-- <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}"> --}}
+@include('include.css')
 </head>
 
 <body class="">
@@ -49,25 +42,8 @@
    @include('include.footer')
   </div>
   </div>
-  <!--   Core   -->
-  <script src="{{ asset('assets/js/plugins/jquery/dist/jquery.min.js') }}"></script>
-  <script src="{{ asset('assets/js/plugins/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
-  <!--   Optional JS   -->
-  <script src="{{ asset('assets/js/plugins/chart.js/dist/Chart.min.js') }}"></script>
-  <script src="{{ asset('assets/js/plugins/chart.js/dist/Chart.extension.js') }}"></script>
-  <!--   Argon JS   -->
-  <script src="{{ asset('assets/js/argon-dashboard.min.js?v=1.1.0') }}"></script>
+@include('include.script')
 
-  {{-- <script src="http://code.jquery.com/jquery-1.10.2.js"></script> --}}
-  {{-- <script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script> --}}
-  <script src="https://cdn.trackjs.com/agent/v3/latest/t.js"></script>
-  <script>
-    window.TrackJS &&
-      TrackJS.install({
-        token: "ee6fab19c5a04ac1a32a645abde4613a",
-        application: "argon-dashboard-free"
-      });
-  </script>
 
   @yield('script')
 </body>
