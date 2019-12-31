677f4925073e1f77ccf65446273489c1c865eb8d
alvian <alvian.ardhiansyah@gmail.com>
2019-12-31 23:55:08 +0700

    reva

--- resources/views/nabung/index.blade.php
+++ resources/views/nabung/index.blade.php
@@ -51,9 +51,25 @@
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
+           <div class="modal-header">
+                <h2 class="modal-title">Menabung</h2>
+            </div>
         <div class="modal-body">
             <form action="">
+                <div class="form-group">
+                    <label for="money">Jumlah uang untuk ditabung</label>
+                    <input type="text" class="form-control" id="money" >
 
+                  </div>
+                  <div class="form-group">
+                    <label for="kurang">Kekurangan</label>
+                    <input type="text" class="form-control" id="kurang" disabled>
+                  </div>
+
+                  <div class="form-group">
+                    <label for="bulan">Jumlah Bulan</label>
+                    <input type="text" class="form-control" id="bulan" disabled>
+                  </div>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
